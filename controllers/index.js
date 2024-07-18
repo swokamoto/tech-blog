3431afe095f3b4e7b7a5142a586c8b8327e3b0cc
swokamoto <swokamoto@gmail.com>
2024-07-17 23:29:34 -0400

    set up handlebars and passed blog data

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,15 @@ const Blog = require('../models/Blog');
 // localhost:3001/api
 router.use('/api', api);
 
+router.get('/', async (req, res) => {
+    const blogData = await Blog.findAll();
+    const blogs = blogData.map((blog) => {
+        return blog.get({ plain: true });
+    })
+    res.render('blog', { blogs } );
+});
+
+
 // localhost:3001/blog
 router.get('/blog', (req, res) => {
     res.json('Hello World');
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const controllers = require('./controllers');
-
+const exphbs = require('express-handlebars');
 
 // instantiate the Blog model
 //const model = require('./models/Blog');
 
-
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
